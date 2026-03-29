c4e99de6af4cd24ca120a023126dd8f78a8e9c0e
Bdon <bcokert@gmail.com>
2026-03-29 15:42:28 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ cp -r .vim ~/.vim
 echo "If you don't have brew yet, make sure to install!"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
 echo "source ~/.aliases" >> ~/.zshrc
-echo "source <(kubectl completion zsh)" >> ~/.zshrc
 
 if [ ! -e ~/.vim/bundles/repos/github.com/Shougo/dein.vim/README.md ]; then
   echo "Installing vim tools, as this is the first time using dotfiles as this user"
